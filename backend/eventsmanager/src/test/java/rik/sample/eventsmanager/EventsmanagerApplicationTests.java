efecda41bba1c4df3611075c961eb2c7d0406244
Jollyroger <roger.suuressaar@gmail.com>
2024-05-10 12:00:18 +0300

    first commit

---
+++ backend/eventsmanager/src/test/java/rik/sample/eventsmanager/EventsmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package rik.sample.eventsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
